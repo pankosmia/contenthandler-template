266c9c5d40a8110d4bb4a523fea0688ee9e09d6a
Gabriel Aillet <aillet.gabriel05@gmail.com>
2026-04-07 13:46:35 +0200

    using pankosmia-rcl

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useContext, useState, useCallback, useEffect } from "react";
 import { Button, Chip, Grid2 } from "@mui/material";
-import { i18nContext, doI18n } from "pithekos-lib";
+import {doI18n } from "pithekos-lib";
+import {i18nContext} from 'pankosmia-rcl'
 import { useNavigate } from "react-router-dom";
 function App() {
   const [maxWindowHeight, setMaxWindowHeight] = useState(
-    window.innerHeight - 64
+    window.innerHeight - 64,
   );
   const handleWindowResize = useCallback((event) => {
     setMaxWindowHeight(window.innerHeight - 64);
@@ -25,7 +26,7 @@ function App() {
         <h1>
           {doI18n(
             "pages:contenthandler-template:stub_content",
-            i18nRef.current
+            i18nRef.current,
           )}
         </h1>
       </Grid2>
@@ -33,13 +34,13 @@ function App() {
         label="bouton create"
         color="secondary"
         variant="outlined"
-         onClick={() => navigate("createPage")}
+        onClick={() => navigate("createPage")}
       />
       <Chip
         label="bouton update"
         color="secondary"
         variant="outlined"
-         onClick={() => navigate("updatePage")}
+        onClick={() => navigate("updatePage")}
       />
     </Grid2>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,28 @@
-import {createRoot} from "react-dom/client";
-import {SpaContainer} from "pithekos-lib";
-import {createHashRouter, RouterProvider} from "react-router-dom";
-import './index.css';
-import CreatePage from "./pages/create";
-import UpdatePage from "./pages/update"
+import { createRoot } from "react-dom/client";
+import { SpaContainer } from "pankosmia-rcl";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+// import CreatePage from "./pages/create";
+// import UpdatePage from "./pages/update";
 import App from "./App";
 
 const router = createHashRouter([
-    {
-        path:"/",
-        element:<App/>
-    },
-    {
-        path: "createPage",
-        element: <CreatePage/>
-    },
-    {
-        path: "updatePage",
-        element: <UpdatePage/>,
-    }
+  {
+    path: "/",
+    element: <App />,
+  },
+  // {
+  //   path: "createPage",
+  //   element: <CreatePage />,
+  // },
+  // {
+  //   path: "updatePage",
+  //   element: <UpdatePage />,
+  // },
 ]);
 
-createRoot(document.getElementById("root"))
-    .render(
-        <SpaContainer>
-            <RouterProvider router={router}/>
-        </SpaContainer>
-    );
+createRoot(document.getElementById("root")).render(
+  <SpaContainer>
+    <RouterProvider router={router} />
+  </SpaContainer>,
+);
